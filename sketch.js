5dce9e67f54811d4b8e3ca23221a3506b20baeb8
rupeshkanth218 <75009562+rupeshkanth218@users.noreply.github.com>
2020-11-27 11:10:25 +0530

    First upload
    Added 2 js files and 1 html tempalate

---
+++ sketch.js
@@ -0,0 +1,24 @@
+const flock=[];
+let alignSlider, cohesionSlider, separationSlider
+function setup()
+{
+    createCanvas(640,360);
+    alignSlider=createSlider(0,5,1,0.1);
+    cohesionSlider=createSlider(0,5,1,0.1);
+    separationSlider=createSlider(0,5,1,0.1);
+    for (let i=0;i<100;i++){
+
+        flock.push(new Boid());
+    }
+}
+
+function draw()
+{
+    background(51);
+    for(let boid of flock){
+        boid.edges();
+        boid.flock(flock);
+        boid.update();
+        boid.show();
+    }
+}
